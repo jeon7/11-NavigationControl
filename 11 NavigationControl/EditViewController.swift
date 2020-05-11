56afe27619d5cfd8b9f3cf156f15ffee5ca5f6cd
Gukhwa Jeon <jeon.gukhwa7@gmail.com>
2020-05-11 15:09:12 +0200

    done

--- 11 NavigationControl/EditViewController.swift
+++ 11 NavigationControl/EditViewController.swift
@@ -8,30 +8,50 @@
 
 import UIKit
 
-
+// 자바의 인터페이스와 흡사
+// 어디에 두는게 맞아? 메인화면 뷰컨드롤러에? 수정화면 뷰컨트롤러에?
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didLampImageSwitchDone(_ controller: EditViewController, isOn: Bool)
+}
 
 class EditViewController: UIViewController {
-    
     var textWayValue: String = ""
     var textMessageValue: String = ""
     var delegate: EditDelegate? // 프로토콜 EditDelegate의 타입변수, 메인화면에서 수정화면으로 넘어올때 이 변수에 자기(메인화면 뷰컨트롤러)를 저장
+    var isOn = false //TODO: 왜 false?
     
     @IBOutlet var lblWay: UILabel!
     @IBOutlet var textMessage: UITextField!
+    @IBOutlet var switchOn: UISwitch!
     
+    // 메인화면 뷰 컨트롤러의 prepare함수가 이 함수 전에 실행됨
+    // 여기 변수값이 prepare함수에서 이미 변경된 상태
     override func viewDidLoad() {
+        print("EditViewController.viewDidLoad() ")
         super.viewDidLoad()
-
+        
         lblWay.text = textWayValue
         textMessage.text = textMessageValue
+        switchOn.isOn = isOn
     }
     
+    // 스위치 상태에 따라 변수값 변경
+    @IBAction func switchLampImg(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        }
+    }
+    
+    // 수정한 데이터를 메인화면으로 전달
     @IBAction func btnDone(_ sender: UIButton) {
-        // 수정한 데이터를 메인화면으로 전달
         // delegate는 메인화면 뷰 컨트롤러. 그러니까 클릭해서 넘어올 수 있는 도입화면
         if delegate != nil {
             // 메인화면 뷰 컨트롤러의 함수를 호출, 거기서 메세지를 받는다
             delegate?.didMessageEditDone(self, message: textMessage.text!)
+            delegate?.didLampImageSwitchDone(self, isOn: isOn)
         }
         // 메인화면으로 돌아가기
         _ = navigationController?.popViewController(animated: true)
--- 11 NavigationControl/ViewController.swift
+++ 11 NavigationControl/ViewController.swift
@@ -8,22 +8,25 @@
 
 import UIKit
 
-// 자바의 인터페이스와 흡사
-// 어디에 두는게 맞아? 메인화면 뷰컨드롤러에? 수정화면 뷰컨드롤러에?
-protocol EditDelegate {
-    func didMessageEditDone(_ controller: EditViewController, message: String)
-}
-
 class ViewController: UIViewController, EditDelegate {
+    
+    let imgOn = UIImage(named: "lamp-on.png")
+    let imgOff = UIImage(named: "lamp-off.png")
+    
+    var isOn = true
   
     @IBOutlet var textMessage: UITextField!
+    @IBOutlet var imgView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imgView.image = imgOn
     }
 
+    // 세그웨이 관련 함수
+    // 수정화면 컨트롤러의 viewDidLoad() 전에 실행
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("ViewController.prepare() ")
         let editViewController = segue.destination as! EditViewController
         if segue.identifier == "editButton" {
             editViewController.textWayValue = "segue: by using button"
@@ -31,14 +34,25 @@ class ViewController: UIViewController, EditDelegate {
             editViewController.textWayValue = "segue: by using bar button"
         }
         editViewController.textMessageValue = textMessage.text!
+        editViewController.isOn = isOn
         editViewController.delegate = self
     }
     
     // 프로토콜의 함수를 구현함
     // 수정화면의 데이터를 메인화면에 보여줌
-    // EditViewController클래스 관련 내용이 함수안에 없는데. 어떻게 동작하니? => prepare에서 delegate변수에 메인화면 뷰컨트롤러 저장
+    // EditViewController클래스 관련 내용이 함수안에 없는데. 어떻게 동작하니? => prepare에서 editViewController의 delegate변수에 메인화면 뷰컨트롤러 저장
     func didMessageEditDone(_ controller: EditViewController, message: String) {
         textMessage.text = message
       }
+    
+    func didLampImageSwitchDone(_ controller: EditViewController, isOn: Bool) {
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        } else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
+    }
 }
 
